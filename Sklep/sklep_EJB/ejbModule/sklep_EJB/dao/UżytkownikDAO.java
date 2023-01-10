321aa0add0326200fceff5ca14471f62dd2dc72d
Jakub Burzej <39655476+Critical31@users.noreply.github.com>
2023-01-10 23:32:04 +0100

    Add files via upload

--- Sklep/sklep_EJB/ejbModule/sklep_EJB/dao/UżytkownikDAO.java
+++ Sklep/sklep_EJB/ejbModule/sklep_EJB/dao/UżytkownikDAO.java
@@ -1,27 +1,41 @@
 package sklep_EJB.dao;
 
-import java.io.Serializable;
+import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.*;
+
+import com.jsf.entities.Person;
 
 
 /**
  * The persistent class for the użytkownik database table.
  * 
  */
-@Entity
-@Table(name="użytkownik")
-@NamedQuery(name="UżytkownikDAO.findAll", query="SELECT u FROM UżytkownikDAO u")
+
 @Stateless
-public class UżytkownikDAO implements Serializable {
-	private static final long serialVersionUID = 1L;
+public class UżytkownikDAO {
 	private static final String UNIT_NAME = "sklep_EJB";
 
 	@PersistenceContext(unitName = UNIT_NAME)
 	protected EntityManager em;
 	
-	public UżytkownikDAO() {
-	}
 
+	public void create(Użytkownik użytkownik) {
+		em.persist(użytkownik);
+		
+	}
+	
+	public Użytkowkik merge(Użytkownik użytkownik) {
+		return em.merge(użytkownik);
+	}
+	
+	public void remove(Użytkownik użytkownik) {
+		em.remove(em.merge(użytkownik));
+	}
+	
+	public Użytkownik find(Object id) {
+		return em.find(Użytkownik.class, id);
+	}
+	
 }
